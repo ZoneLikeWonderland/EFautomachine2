4531008ddf5b2197fd5afd3a004688a6b6bc7668
ZarkLngeW <zhanglw2@shanghaitech.edu.cn>
2019-12-08 16:49:28 +0800

    ajust address

---
+++ EFec.user.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         EF automachine
+// @namespace    http://tampermonkey.net/
+// @version      0.3
+// @description  This script will send final status to the server
+// @author       ZarkLngeW
+// @match        https://ec.ef.com.cn/school/studyplan*
+// @grant        none
+// ==/UserScript==
+
+function polling() {
+    try {
+        if ($("div[class='ets-ui-acc-bd-main']")[0].childElementCount > 0) {
+            console.log("submit")
+            submit()
+        }
+    } catch (err) {
+        console.log(err)
+    }
+    setTimeout(function () {
+        console.log("waiting")
+        polling();
+    }, 2000)
+}
+
+function submit() {
+    $("li[data-at-id][class^='ets-ui-acc-act-nav-act']").each(function () {
+        aid = this.attributes["data-at-id"].value.match(/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/)[0]
+        console.log(aid)
+        r = $.ajax({
+            type: "POST",
+            url: "https://ec.ef.com.cn/services/api/school/command/scoring/submitactivityscore?c=countrycode%3dcn%7cculturecode%3dzh-CN%7cpartnercode%3dCncp%7csiteversion%3d20-1%7cstudentcountrycode%3dcn%7cdevicetypeid%3d1%7cproductid%3d100",
+            data: JSON.stringify({ "studentActivityId": aid, "score": 100, "minutesSpent": parseInt(Math.random() * 10 + 1), "studyMode": 0 }),
+            contentType: "application/json"
+        })
+        console.log(r)
+    })
+}
+
+polling()
--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # EFautomachine2
 So stupid version 1!
-Click [HERE](https://github.com/ZoneLikeWonderland/EFautomachine2/raw/master/EF.user.js) to install.
+
+Click [HERE](https://github.com/ZoneLikeWonderland/EFautomachine2/raw/master/EF.user.js) to install for `https://corporate.ef.com.cn`.
+
+Click [HERE](https://github.com/ZoneLikeWonderland/EFautomachine2/raw/master/EFec.user.js) to install for `https://ec.ef.com.cn`.
 
 # Known bug
 Sending requests too fast, this script may cause that the result pages don't recognize your accomplishment.
